70789b1ebd5da9b500b7d9affafdb10896ddddeb
lukas <lukas.nick5@gmail.com>
2022-12-12 22:08:32 +0100

    changes

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,23 @@ function togglemenu() {
      mobile_menu.classList.toggle('is-active');
 }
 menu_btn.addEventListener('click', togglemenu);
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+     const currentScroll = window.pageYOffset
+     if (currentScroll <= 0){
+          body.classList.remove("scroll-up")
+     }
+     if (currentScroll > (lastScroll+10) && !body.classList.contains("scroll-down")){
+          body.classList.remove("scroll-up")
+          body.classList.add("scroll-down")
+     }
+     if (currentScroll < lastScroll && body.classList.contains("scroll-down")){
+          body.classList.remove("scroll-down")
+          body.classList.add("scroll-up")
+     }
+
+     lastScroll = currentScroll;
+})
